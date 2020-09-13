355d50e5292bf0ceb6e3e2a694189528090b637c
kaiorr <kaio.rocharibeiro@gmail.com>
2020-09-13 19:33:25 -0300

    feat: telas finalizadas, resta corrigir alteração de estado nos favoritos

--- event/lib/main.dart
+++ event/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:event/palestras.dart';
 import 'package:flutter/material.dart';
 import 'package:event/event.dart';
 
@@ -25,70 +26,81 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: <Widget>[
-        Container(
-          height: MediaQuery.of(context).size.height * 0.45,
-          width: MediaQuery.of(context).size.width,
-          padding: EdgeInsets.only(bottom: 15),
-          child: Align(
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height * 0.45,
+            width: MediaQuery.of(context).size.width,
+            padding: EdgeInsets.only(bottom: 15),
+            child: Align(
               alignment: Alignment.bottomCenter,
               child: Text(
-                'Flutter Conf 2020',
+                'Rock in Rio',
                 style: TextStyle(
-                    fontFamily: 'Righteous', fontSize: 50, color: Colors.white),
-              )),
-          decoration: BoxDecoration(
+                  fontFamily: 'Righteous',
+                  fontSize: 50,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            decoration: BoxDecoration(
               image: DecorationImage(
-                  fit: BoxFit.cover, image: AssetImage('assets/flutter.jpg'))),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        RowInfo(),
-        Divider(),
-        SizedBox(
-          height: 30,
-        ),
-        RowDicas('Beba Água'),
-        SizedBox(
-          height: 15,
-        ),
-        RowDicas('Compartilhe Conhecimento'),
-        Spacer(),
-        SizedBox(
-          height: 15,
-        ),
-        RowDicas('Conhecimento e Prática'),
-        SizedBox(
-          height: 15,
-        ),
-        Spacer(),
-        RowDicas('#Flutter nas redes sociais'),
-        Spacer(),
-        RaisedButton(
-          color: Colors.blue.shade500,
-          textColor: Colors.white,
-          padding: EdgeInsets.symmetric(vertical: 12, horizontal: 80),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(80)),
+                fit: BoxFit.cover,
+                image: AssetImage(
+                  'assets/party.jpg',
+                ),
+              ),
+            ),
           ),
-          onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (context) {
-              return EventScreen();
-            }));
-          },
-          child: Text(
-            'Palestras',
-            style: TextStyle(
-                fontSize: 24, fontWeight: FontWeight.w800, letterSpacing: 3),
+          SizedBox(
+            height: 10,
           ),
-        ),
-        SizedBox(
-          height: 40,
-        ),
-      ],
-    ));
+          RowInfo(),
+          Divider(),
+          SizedBox(
+            height: 30,
+          ),
+          RowDicas('Beba água'),
+          SizedBox(
+            height: 15,
+          ),
+          RowDicas('Compartilhe Conhecimento'),
+          SizedBox(
+            height: 15,
+          ),
+          RowDicas('Conhecimento e Prática'),
+          Spacer(),
+          RowDicas('#FlutterLatamConf nas redes sociais'),
+          Spacer(),
+          RaisedButton(
+            color: Colors.blue.shade900,
+            textColor: Colors.white,
+            padding: EdgeInsets.symmetric(vertical: 12, horizontal: 80),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(50),
+              ),
+            ),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return HomeScreen();
+              }));
+            },
+            child: Text(
+              'Palestras',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+                letterSpacing: 3,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+        ],
+      ),
+    );
   }
 }
 
@@ -169,9 +181,102 @@ class _EventScreentate extends State<EventScreen> {
     var listView = ListView.builder(
       itemCount: listaEvent.length,
       itemBuilder: (context, index) {
-        final event = _listaEvent[index];
+        final event = listaEvent[index];
         bool _isFavorito = _listaFavoritos.contains(event);
+        return ListTile(
+          title: Text(event.empresa),
+          subtitle: Text(event.assunto),
+          leading: Text(event.horario),
+          trailing: IconButton(
+            icon: _isFavorito
+                ? Icon(Icons.favorite, color: Colors.red)
+                : Icon(
+                    Icons.favorite_border,
+                  ),
+            onPressed: () {
+              setState(() {
+                if (_isFavorito) {
+                  _listaFavoritos.remove(listaEvent[index]);
+                } else {
+                  _listaFavoritos.add(listaEvent[index]);
+                }
+              });
+            },
+          ),
+        );
       },
     );
+    return Scaffold(
+      appBar: AppBar(title: Text('Palestras'), actions: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.favorite,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return AgendaScreen(listaFavoritos: _listaFavoritos);
+            }));
+          },
+        )
+      ]),
+      body: listView,
+    );
+  }
+}
+
+class AgendaScreen extends StatefulWidget {
+  final List<Event> listaFavoritos;
+  AgendaScreen({this.listaFavoritos});
+
+  _AgendaScreenState createState() => _AgendaScreenState();
+}
+
+class _AgendaScreenState extends State<AgendaScreen> {
+  TextStyle _style = TextStyle(
+    fontSize: 26,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Agenda'),
+        ),
+        body: ListView.builder(
+            padding: const EdgeInsets.all(14.0),
+            itemCount: widget.listaFavoritos.length,
+            itemBuilder: (context, index) {
+              final event = widget.listaFavoritos[index];
+              return Card(
+                  child: Column(children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          event.empresa,
+                          style: _style,
+                        ),
+                        Text(
+                          event.horario,
+                          style: _style,
+                        ),
+                      ],
+                    )),
+                Divider(),
+                Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Icon(Icons.thumb_up),
+                        Icon(Icons.comment),
+                        Icon(Icons.share),
+                      ],
+                    ))
+              ]));
+            }));
   }
 }
---
+++ event/lib/palestras.dart
@@ -0,0 +1,44 @@
+// 20:10 - 20:30 IATEC
+// 20:30 - 21:00 @Rodrigo Branas
+// 21:00 - 21:20 Will Bank
+// 21:20 - 21:40 @iFood
+import 'package:event/event.dart';
+
+// Contém a lista de shows disponíveis.
+final List<Event> listaEvent = [
+  const Event(
+    empresa: 'Fluterando',
+    horario: '19:00 - 19:30',
+    assunto: 'Evento Principal',
+  ),
+  const Event(
+    empresa: 'LarApp',
+    horario: '19:30 - 19:50',
+    assunto: 'Utilização do flutter na construção do App',
+  ),
+  const Event(
+    empresa: 'CloudWalk',
+    horario: '19:50 - 20:10',
+    assunto: '',
+  ),
+  const Event(
+    empresa: 'IATEC',
+    horario: '20:10 - 20:30',
+    assunto: 'Utilização do flutter na construção do App',
+  ),
+  const Event(
+    empresa: 'CloudWalk',
+    horario: '19:50 - 20:10',
+    assunto: '',
+  ),
+  const Event(
+    empresa: 'LarApp',
+    horario: '19:30 - 19:50',
+    assunto: 'Utilização do flutter na construção do App',
+  ),
+  const Event(
+    empresa: 'CloudWalk',
+    horario: '19:50 - 20:10',
+    assunto: '',
+  ),
+];
