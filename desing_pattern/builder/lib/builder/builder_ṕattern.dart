ee6bf688be1d9f5b3ed15858cec62afd0e03a76e
kaiorr <kaio.rocharibeiro@gmail.com>
2020-09-22 01:35:25 -0300

    feat: finalizado CustomDioBuilder com requisicoes na api placeholder

--- desing_pattern/builder/lib/builder/builder_ṕattern.dart
+++ desing_pattern/builder/lib/builder/builder_ṕattern.dart
@@ -1,7 +1,8 @@
 import 'package:builder/builder/funcionario_builder.dart';
+import 'package:builder/dio/custom_dio_builder.dart';
 import 'package:builder/model/filho_model.dart';
 
-void execute() {
+Future<void> execute() async {
   var FuncionarioImpressao = FuncionarioBuilder.instance
       .nome("Kaio Ribeiro")
       .idade(33)
@@ -9,4 +10,30 @@ void execute() {
       .addFilho(FilhoModel(nome: 'Anna Vitória', idade: 3))
       .build();
   print(FuncionarioImpressao);
+
+  print('########### ABAIXO IMPLEMENTACAO DO CUSTOM DIO ############');
+
+  print('Method: POST');
+  var response = await CustomDioBuilder.instance
+      .post()
+      .path('http://jsonplaceholder.typicode.com/posts')
+      .addHeader('Content-Type', 'application/json')
+      .params()
+      .data({'titile': 'titulo', 'body': 'Corpo', 'userId': 1}).run();
+  var resultPost = response;
+
+  print(resultPost.data);
+
+  print('#################################################');
+
+  print('Method: GET');
+  var resultGet = await CustomDioBuilder.instance
+      .get()
+      .path('http://jsonplaceholder.typicode.com/posts')
+      .params()
+      .queryParameters()
+      .addQueryParam('userId', 1)
+      .run();
+
+  print(resultGet.data);
 }
